4d538fe2f2240652c40753b452144ea650cf4e28
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-03-13 09:25:59 +0300

    concrete views created

--- _books/api/urls.py
+++ _books/api/urls.py
@@ -4,4 +4,7 @@ from _books.api import views as api_views
 urlpatterns = [
     path('books/',api_views.BookListCreateApiView.as_view(), name='Book-List'),
     path('comments/',api_views.CommentListCreateApiView.as_view(), name='Comment-List'),
+
+    path('books/<int:pk>',api_views.BookDetailApiView.as_view(), name='Book-Detail'),
+    path('comments/<int:pk>',api_views.CommentDetailApiView.as_view(), name='Comment-Detail'),
 ]
--- _books/api/views.py
+++ _books/api/views.py
@@ -1,30 +1,49 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
+from rest_framework import generics
+
 from _books.api.serializers import BookSerializer, CommentSerializer
 from _books.models import Book, Comment
 
-class BookListCreateApiView(ListModelMixin, CreateModelMixin, GenericAPIView):
-
+class BookListCreateApiView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def get(self,request,*args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self,request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class CommentListCreateApiView(ListModelMixin, CreateModelMixin, GenericAPIView):
-
+class CommentListCreateApiView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-    def get(self,request,*args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class BookDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def post(self,request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class CommentDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+# class BookListCreateApiView(ListModelMixin, CreateModelMixin, GenericAPIView):
+
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+#     def get(self,request,*args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self,request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
+# class CommentListCreateApiView(ListModelMixin, CreateModelMixin, GenericAPIView):
+
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+#     def get(self,request,*args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self,request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
     
 
 
