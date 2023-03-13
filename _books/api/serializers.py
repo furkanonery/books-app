95b3470860e167f51f93e1f24d91f57006392b87
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-03-13 08:04:22 +0300

    serializers created

---
+++ _books/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from _books.models import Book, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    Comments = CommentSerializer(many=True, read_only=True)
+    class Meta:
+        model = Book
+        fields = '__all__'
---
+++ _books/api/views.py
@@ -0,0 +1,1 @@
+from _books.api.serializers import BookSerializer, CommentSerializer
